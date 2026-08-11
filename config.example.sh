eb85ad9386929a27cf04320d54181a07a2cdc2a3
axenl <155933069+axenl@users.noreply.github.com>
2026-08-11 15:03:28 +0800

    Update config.example.sh

--- config.example.sh
+++ config.example.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Website directories
 WEBSITES=(
 "/www/wwwroot/example.com"
 )
 
+# Notification configuration
+# Do not commit real tokens
 WECHAT_WEBHOOK=""
 
+# Monitoring options
 CHECK_PHP_FPM=true
 CHECK_FILE_CHANGE=true
 CHECK_JOOMLA=true
